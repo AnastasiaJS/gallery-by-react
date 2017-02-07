4a25e34f8a3fc93544892bc6ce26a7c39aeee9f4
AnastasiaJS <245915765@qq.com>
2017-02-07 15:12:51 +0800

    [Add]项目构建、舞台基本架构

---
+++ src/components/Main.js
@@ -0,0 +1,39 @@
+require('normalize.css/normalize.css');
+require('styles/App.scss');
+
+import React from 'react';
+
+let imageDatas = require('../data/imageData.json');
+
+
+imageDatas=(function genImageURL(imageDatasArr) {
+  for(let i=0;i<imageDatasArr.length;i++){
+    let singleImageData=imageDatasArr[i];
+
+    singleImageData.imageURL=require('../images/'+singleImageData.fileName);
+
+    imageDatasArr[i]=singleImageData;
+  }
+
+  return imageDatasArr;
+})(imageDatas);
+
+class AppComponent extends React.Component {
+  render() {
+    return (
+      <section className="stage">
+        <section className="image-sec">
+
+        </section>
+        <nav className="controller-nav">
+
+        </nav>
+      </section>
+    );
+  }
+}
+
+AppComponent.defaultProps = {
+};
+
+export default AppComponent;
